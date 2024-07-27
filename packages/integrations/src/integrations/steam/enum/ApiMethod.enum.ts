cdc26704a22a8d606a3c91ef2d031d439061891e
Алексей <kuckova6@gmail.com>
2024-07-27 17:36:26 +0300

    add new api method

--- packages/integrations/src/integrations/steam/enum/ApiMethod.enum.ts
+++ packages/integrations/src/integrations/steam/enum/ApiMethod.enum.ts
@@ -1,4 +1,5 @@
 export const enum ApiMethod {
     GET_PLAYER_SUMMARIES = 'ISteamUser/GetPlayerSummaries/v0002',
     GET_PLAYER_BANS = 'ISteamUser/GetPlayerBans/v1',
+    RESOLVE_URL = 'ISteamUser/ResolveVanityURL/v0001',
 }
